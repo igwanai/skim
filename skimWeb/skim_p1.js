641063a19f786a7418dc8b4594d7276e32b3e8cb
Jian-Nam <jeeahn2929@gmail.com>
2022-01-22 12:39:16 +0900

    p1) back 버튼 이전버전으로 덮어씌워져서 다시 갱신함

--- skimWeb/skim_p1.js
+++ skimWeb/skim_p1.js
@@ -157,13 +157,13 @@ window.addEventListener("resize", function(){
 let button = document.getElementById("button");
 
 button.onmouseenter = function() { //버튼 밖에서 안으로 마우스가 올라왔을 떄
-    button.innerHTML = "(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)";
+    button.innerHTML = "(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;b&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;k)";
 };
 button.onpointerleave = function() { //버튼 안에서 밖으로 마우스가 떠났을 때
     button.innerHTML = '(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;back)';
 }
 button.onclick = function() { //버튼을 클릭했을 때
-    button.innerHTML = "(&nbsp;&nbsp;o)";
+    button.innerHTML = '(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;b<span style="color:red;">ac</span>k)';
     location.href='index.html';
 }
 
