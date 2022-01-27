949e841dcca2b74628d6e38316647d4cde7580ee
Jian-Nam <jeeahn2929@gmail.com>
2022-01-28 01:59:33 +0900

    로컬서버 호스팅, p1) 마우스휠 인터렉션

---
+++ skimWeb/config/db.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "3877", 
+    database: "db"
+})
+
+db.connect();
+
+class sensor{
+    static async getValue(){
+        return new Promise((resolve, reject) => {
+            db.query("SELECT num FROM data WHERE num = 100", (err, data)=>{
+                if(err) reject(err);
+                resolve(data[0]);
+            });   
+        });
+    }
+}
+
+module.exports = {
+    sensor,
+}
---
+++ skimWeb/js/main.js
@@ -0,0 +1,18 @@
+var p1_button = document.querySelector(".project1");
+var p2_button = document.querySelector(".project2");
+var p3_button = document.querySelector(".project3");
+var works_button = document.querySelector(".work");
+var about_button = document.querySelector(".about");
+
+p1_button.addEventListener("click", go_p1);
+p2_button.addEventListener("click", go_p2);
+p3_button.addEventListener("click", go_p3);
+works_button.addEventListener("click", go_works);
+about_button.addEventListener("click", go_about);
+
+function go_p1(){location.href = "/skim_project?num=1";};
+function go_p2(){location.href = "/skim_project?num=2";};
+function go_p3(){location.href = "/skim_project?num=3";};
+
+function go_works(){location.href = "/work";};
+function go_about(){location.href = "/about";};
